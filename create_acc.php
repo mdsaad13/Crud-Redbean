72496a3ad37b51337457bd373a9e275e4bfd01f6
Mohammed Saad <saad.surana@gmail.com>
2019-09-28 22:56:20 +0530

    new pages added

---
+++ create_acc.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <div class="container">
+  <h1>create new account</h1>
+  </div>
+
+<div class="container">
+
+  <form class="px-4 py-3" action="controllers/createacc_control.php" method="post">
+  <div class="form-group">
+      <label for="exampleDropdownFormname1">first name</label>
+      <input type="text" class="form-control" id="exampleDropdownFormname" placeholder= "Name"firname="firstname">
+    </div>
+    <div class="form-group">
+    <label for="exampleDropdownFormname1">last name</label>
+      <input type="text" class="form-control" id="exampleDropdownFormname" placeholder=" Name"lasname="lastname">
+    </div>
+    <div class="form-group">
+    <label for="exampleDropdownFormname1">Adress</label>
+      <input type="text" class="form-control" id="exampleDropdownFormname" placeholder="Adress" name="address">
+    </div>
+    <div class="form-group">
+    <label for="exampleDropdownFormname1">mobile no</label>
+      <input type="text" class="form-control" id="exampleDropdownFormname" placeholder="mobile no"  name="mobileno">
+    </div>
+    <div class="form-group">
+      <label for="exampleDropdownFormEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com" name="email">
+    </div>
+    <div class="form-group">
+      <label for="exampleDropdownFormPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password"name="password" >
+    </div>
+    <div class="form-check">
+      <input type="checkbox" class="form-check-input" id="dropdownCheck">
+      <label class="form-check-label" for="dropdownCheck">
+        Remember me
+      </label>
+    </div>
+    <input type="submit" value="SUBMIT">
+    <button type="submit" class="btn btn-primary">Sign in</button>
+  </form>
+  <div class="dropdown-divider"></div>
+  <a class="dropdown-item" href="#">New around here? Sign up</a>
+  <a class="dropdown-item" href="#">Forgot password?</a>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
+  </body>
+</html>
